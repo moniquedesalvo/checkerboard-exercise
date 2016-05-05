4aff2e6e13f36bf05bbd6da217c0565101a85fec
moniquedesalvo <moniquerenee@gmail.com>
2016-05-05 11:57:08 -0700

    red and black checkerboard

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-//Gradient
 function makeCheckerBoard() {
 	for (var i = 0; i < 63; i++) {
-		var tile = document.createElement('div');
-			tile.style.width = "11.1%";
-			tile.style.float = "left";
-			tile.style.paddingBottom = "11.1%";
-			tile.style.opacity = (i + 10) / 63;
-			document.body.style.backgroundColor = 'blue';
-		if (i % 2 === 0) {
-			tile.style.backgroundColor = 'purple';
-		} else {
-			tile.style.backgroundColor = 'cyan';
-		}		
-		document.body.appendChild(tile);
-	}
-}
-makeCheckerBoard();
+ 		var tile = document.createElement('div');
+		tile.style.width = "11.1%";
+		tile.style.float = "left";
+		tile.style.paddingBottom = "11.1%";
+	if (i % 2 === 0) {
+ 			tile.style.backgroundColor = "red";
+ 		} else {
+ 			tile.style.backgroundColor = "black";
+ 		}
+ 	document.body.appendChild(tile);
+ 	}
+ }
+ makeCheckerBoard()
+ 
