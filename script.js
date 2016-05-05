07fdd3b5d049cc7105324b545dc06033fdd06969
moniquedesalvo <moniquerenee@gmail.com>
2016-05-05 11:50:45 -0700

    committing gradient branch

--- script.js
+++ script.js
@@ -1,26 +1,18 @@
-//Random Colors
-var hexString = "0123456789ABCDEF"
-
-function colorGenerator(hexString) {
-	var hexCode = ['#'];
-	for (var i = 0; i < 6; i++) {
-		var random = Math.floor(Math.random() * 16);
-		hexCode.push(hexString[random]);
-	}
-	return hexCode.join('');
-}
-
-colorGenerator(hexString)
-
+//Gradient
 function makeCheckerBoard() {
 	for (var i = 0; i < 63; i++) {
 		var tile = document.createElement('div');
-		tile.style.width = '11.1%';
-		tile.style.float = 'left';
-		tile.style.paddingBottom = '11.1%';
-		tile.style.backgroundColor = colorGenerator(hexString);
+			tile.style.width = "11.1%";
+			tile.style.float = "left";
+			tile.style.paddingBottom = "11.1%";
+			tile.style.opacity = (i + 10) / 63;
+			document.body.style.backgroundColor = 'blue';
+		if (i % 2 === 0) {
+			tile.style.backgroundColor = 'purple';
+		} else {
+			tile.style.backgroundColor = 'cyan';
+		}		
 		document.body.appendChild(tile);
 	}
 }
-
 makeCheckerBoard();
