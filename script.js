de5983bcc4a791ad791a5e810d5f5103a49ad756
moniquedesalvo <moniquerenee@gmail.com>
2016-05-05 08:50:31 -0700

    random color checkerboard

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-//Red and Black
+//Random Colors
+var hexString = "0123456789ABCDEF"
+
+function colorGenerator(hexString) {
+	var hexCode = ['#'];
+	for (var i = 0; i < 6; i++) {
+		var random = Math.floor(Math.random() * 16)
+		hexCode.push(hexString[random])
+	}
+	return hexCode.join('');
+}
+
+console.log(colorGenerator(hexString))
+
 function makeCheckerBoard() {
 	for (var i = 0; i < 63; i++) {
 		var tile = document.createElement('div');
-			tile.style.width = "11.1%";
-			tile.style.float = "left";
-			tile.style.paddingBottom = "11.1%";
-		if (i % 2 === 0) {
-			tile.style.backgroundColor = "red";
-		} else {
-			tile.style.backgroundColor = "black";
-		}
-	document.body.appendChild(tile);
+		tile.style.width = '11.1%';
+		tile.style.float = 'left';
+		tile.style.paddingBottom = '11.1%';
+		tile.style.backgroundColor = colorGenerator(hexString);
+		document.body.appendChild(tile);
 	}
 }
-makeCheckerBoard()
-
-
