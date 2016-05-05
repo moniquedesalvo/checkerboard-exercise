41ef04962af7e38668ab7974c05dea817c543740
moniquedesalvo <moniquerenee@gmail.com>
2016-05-05 08:44:30 -0700

    committing red and black checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// Your JS goes here
+//Red and Black
+function makeCheckerBoard() {
+	for (var i = 0; i < 63; i++) {
+		var tile = document.createElement('div');
+			tile.style.width = "11.1%";
+			tile.style.float = "left";
+			tile.style.paddingBottom = "11.1%";
+		if (i % 2 === 0) {
+			tile.style.backgroundColor = "red";
+		} else {
+			tile.style.backgroundColor = "black";
+		}
+	document.body.appendChild(tile);
+	}
+}
+makeCheckerBoard()
+
+
